6934423c8ee9b5109479786b9f56929fa0c191d2
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-11-03 15:27:30 +0100

    Do the test

--- packages/orthodox/test.js
+++ packages/orthodox/test.js
@@ -1,2 +1,15 @@
 const orthodoxSpec = require('orthodox-spec/spec.json');
 const spec = require('tape-catch');
+const orthodox = require('.');
+
+orthodoxSpec.forEach(testData => {
+  spec(testData.what, (test) => {
+    test.equal(
+      orthodox(testData.input),
+      testData.output,
+      'real output matches expected `output`'
+    );
+
+    test.end();
+  });
+});
